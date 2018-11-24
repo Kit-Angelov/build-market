cb609780120e9429b53b4b5af3c7424b4a3f385a
Kit-Angelov <kit.angelov@gmail.com>
2018-11-24 22:27:11 +0300

    add product tags model

--- app/utils.py
+++ app/utils.py
@@ -30,3 +30,40 @@ def tag_parser_service(service):
         tag_service = TagService.objects.get_or_create(text=elem)
         tag_service.service.add(service)
         tag_service.save()
+
+
+def tag_parser_product(product):
+    name = product.name
+    features = product.productfeature_set.all()
+    store = product.store
+
+    tags = list()
+
+    # parse name
+    tags.append(str(name).lower())
+
+    for name_elem in str(name).split():
+        tags.append(str(name_elem).lower())
+
+    # parse features
+    for feature in features:
+        feature_title = feature.title
+        feature_text = feature.text
+
+        tags.append(str(feature_title).lower())
+        tags.append(str(feature_text).lower())
+
+        for title_elem in str(feature_title).split():
+            tags.append(str(title_elem).lower())
+        for text_elem in str(feature_text).split():
+            tags.append(str(text_elem).lower())
+
+    # parse store
+    tags.append(str(store.name).lower())
+    for name_elem in str(store.name).split():
+        tags.append(str(name_elem).lower())
+
+    for elem in tags:
+        tag_product = TagProduct.objects.get_or_create(text=elem)
+        tag_product.service.add(product)
+        tag_product.save()
